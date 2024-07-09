39dbf245df8aa2b0cde3951510cde81eb8bf8a3a
Maxim Veretelnikov <generalfay@mail.ru>
2024-07-09 16:59:56 +0700

    init

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import {useEffect, useState} from 'react'
+import './App.scss'
+import fetchCards from "./fetchCards.js";
+
+
+function App() {
+
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default App
